3a9a70bb88f26f1a3ed2377319448e2eb7d8d05d
gitceiMac05 <gitce123>
2018-11-29 08:56:49 -0500

    Adicion de campos faltantes de la tabla Congresos

--- database/migrations/2018_11_15_164707_create_congresos_table.php
+++ database/migrations/2018_11_15_164707_create_congresos_table.php
@@ -16,11 +16,13 @@ class CreateCongresosTable extends Migration
         Schema::create('congresos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
+            $table->integer ('edicion');
             $table->integer('year');
             $table->date('fecha_inicio');
             $table->date('fecha_final');
             $table->string('pais');
             $table->string('lugar');
+            $table->string ('descripcion');
             $table->timestamps();
         });
     }
